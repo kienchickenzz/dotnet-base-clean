3d471d644dbb62cc9563620bb751c9c49e7d083a
Nguyen Duc Kien <kienchickenz@gmail.com>
2026-03-19 19:34:10 +0700

    Setup Domain

--- API/BaseCleanArchitecture.Api/Program.cs
+++ API/BaseCleanArchitecture.Api/Program.cs
@@ -1,6 +1,50 @@
+using BaseCleanArchitecture.Application;
+using BaseCleanArchitecture.Persistence;
+using BaseCleanArchitecture.Persistence.Initialization;
+using BaseCleanArchitecture.Api.Configurations;
+using BaseCleanArchitecture.Api.Extensions;
+using BaseCleanArchitecture.Api.OpenApi;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddHttpContextAccessor();
+
+builder.AddConfigurations();
+builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
+builder.Services.AddApiServices();
+builder.Services.AddApplication();
+
+builder.Services.AddInfrastructurePersistence(builder.Configuration);
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Initialize database (migrate + seed)
+await app.Services.InitializeDatabaseAsync();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSwaggerExtension();
+
+// app.UseHttpsRedirection(); // Disable for dev
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.UseCustomExceptionHandler();
+
 
 app.Run();
---
+++ Core/Common/ISoftDelete.cs
@@ -0,0 +1,8 @@
+﻿namespace BaseCleanArchitecture.Domain.Common;
+
+
+public interface ISoftDelete
+{
+    DateTime? DeletedOn { get; set; }
+    int? DeletedBy { get; set; }
+}
